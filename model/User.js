554184230acb2b5a22af44ccfb892f548e3dfc8e
julia <77528734+juliagronau@users.noreply.github.com>
2022-01-06 13:37:43 +0100

    add cast: false to user schema

--- model/User.js
+++ model/User.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  first_name: { type: String, required: true, cast: false }, //cast: false bewirkt, dass der value nicht automatisch zum String gecastet wird, sondern akzeptiert ihn nur, wenn er bereits ein String ist
+  last_name: { type: String, required: true, cast: false },
   age: { type: Number },
 });
 
